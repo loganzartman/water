e8176f6c0b62043c15babeba4b3084a431f9cf7e
nondefault <spam@nondefault.net>
2022-12-05 18:03:15 -0800

    draw a triangle

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { hot } from '@use-gpu/live';
 import { AutoCanvas, WebGPU } from '@use-gpu/webgpu';
 import { Overlay } from './Overlay';
 import { HTML } from '@use-gpu/react';
+import { Data, Draw, FaceLayer, Pass } from '@use-gpu/workbench';
+import type {StorageSource, DataField} from '@use-gpu/core';
 
 const makeFallback = (error: Error) =>
   <div className="error-modal">
@@ -22,7 +24,36 @@ export const App = hot(() => {
         selector={'#root .canvas'}
         samples={4}
       >
+        <TestTriangle />
       </AutoCanvas>
     </WebGPU>
   )
 }, module);
+
+const TestTriangle = () => {
+  const positions: number[] = [
+    -1.0, -1.0,
+    1.0, -1.0,
+    0.0, 1.0,
+  ];
+  const colors: number[] = [
+    1.0, 0.0, 0.0,
+    0.0, 1.0, 0.0,
+    0.0, 0.0, 1.0,
+  ];
+  const fields: DataField[] = [
+    ['vec2<f32>', positions],
+    ['vec3<f32>', colors],
+  ];
+
+  return (
+    <Draw>
+      <Pass>
+        <Data
+          fields={fields}
+          render={(positions: StorageSource, colors: StorageSource) => <FaceLayer positions={positions} colors={colors} />}
+        />
+      </Pass>
+    </Draw>
+  )
+}
